91bad2f062ea443e26f187dc9bce6427a5e7d347
Alley-eddine <alleybenhassine@gmail.com>
2025-10-27 16:47:44 +0100

    refacto login

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,29 +2,7 @@
   <div>
     <AppNavbar />
     <div class="page-wrapper">
-      <div class="form-card">
-        <h2 class="form-title">Connexion</h2>
-
-        <div class="input-group">
-          <InputGroup>
-            <InputGroupAddon>
-              <i class="pi pi-user" />
-            </InputGroupAddon>
-            <InputText v-model="email" placeholder="Email" />
-          </InputGroup>
-        </div>
-
-        <div class="input-group">
-          <InputGroup>
-            <InputGroupAddon>
-              <i class="pi pi-lock" />
-            </InputGroupAddon>
-            <InputText v-model="password" placeholder="Mot de passe" type="password" />
-          </InputGroup>
-        </div>
-
-        <Button class="button" label="Connexion" />
-      </div>
+      <LoginForm />
     </div>
   </div>
 </template>
