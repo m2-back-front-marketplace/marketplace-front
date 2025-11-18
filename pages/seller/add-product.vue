59f56fc13625669a19464dd2b4e53e974d863f46
ThomasGrare <thomasgrare64@gmail.com>
2025-11-18 14:57:56 +0100

    feat: implement productForm

---
+++ pages/seller/add-product.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Créer un produit</h2>
+    <ProductForm @submit="onCreate" />
+  </div>
+</template>
+
+<script setup>
+import ProductForm from "~/components/form/ProductForm.vue";
+
+async function onCreate(payload) {
+  // Exemple: envoi à ton API
+  console.log("Submit payload:", payload);
+  // await $fetch('/api/products', { method: 'POST', body: payload })
+}
+definePageMeta({
+  layout: "seller",
+});
+</script>
