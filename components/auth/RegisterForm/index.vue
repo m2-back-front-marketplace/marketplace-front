7e1c5f65ba28d3242a1ff6f65c8ee344666532d6
remy <remy.berjonneau@gmail.com>
2025-12-09 22:59:06 +0100

    fix: add username value in form

--- components/auth/RegisterForm/index.vue
+++ components/auth/RegisterForm/index.vue
@@ -1,144 +1,195 @@
 <template>
-  <form class="form-card" @submit.prevent="onSubmit">
-    <h2 class="form-title">Vous Êtes ?</h2>
-    <div class="segmented">
-      <button
-        type="button"
-        class="seg-btn"
-        :class="{ active: userType === 'pro' }"
-        @click="userType = 'pro'"
-      >
-        Un Professionnel
-      </button>
-      <button
-        type="button"
-        class="seg-btn"
-        :class="{ active: userType === 'part' }"
-        @click="userType = 'part'"
-      >
-        Un Particulier
-      </button>
-    </div>
+    <form class="form-card" @submit.prevent="onSubmit">
+        <h2 class="form-title">Vous Êtes ?</h2>
+        <div class="segmented">
+            <button
+                type="button"
+                class="seg-btn"
+                :class="{ active: userType === 'pro' }"
+                @click="userType = 'pro'"
+            >
+                Un Professionnel
+            </button>
+            <button
+                type="button"
+                class="seg-btn"
+                :class="{ active: userType === 'part' }"
+                @click="userType = 'part'"
+            >
+                Un Particulier
+            </button>
+        </div>
 
-    <!-- PRO -->
-    <template v-if="userType === 'pro'">
-      <div class="input-group">
-        <label>Nom de société</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-briefcase"></i></InputGroupAddon>
-          <InputText v-model="pro.company" placeholder="Nom de société" />
-        </InputGroup>
-      </div>
+        <!-- PRO -->
+        <template v-if="userType === 'pro'">
+            <div class="input-group">
+                <label>Nom de société</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-briefcase"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="pro.company"
+                        placeholder="Nom de société"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Email</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-envelope"></i></InputGroupAddon>
-          <InputText v-model="pro.email" placeholder="Email" type="email" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Username</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-briefcase"></i
+                    ></InputGroupAddon>
+                    <InputText v-model="pro.Username" placeholder="Username" />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Mot de passe</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-lock"></i></InputGroupAddon>
-          <InputText
-            v-model="pro.password"
-            type="password"
-            placeholder="Mot de passe"
-          />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Email</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-envelope"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="pro.email"
+                        placeholder="Email"
+                        type="email"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Numéro de SIRET</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-id-card"></i></InputGroupAddon>
-          <InputNumber v-model="pro.siret" placeholder="Numéro de SIRET" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Mot de passe</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-lock"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="pro.password"
+                        type="password"
+                        placeholder="Mot de passe"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Numéro de TVA</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-percentage"></i></InputGroupAddon>
-          <InputNumber v-model="pro.vat" placeholder="Numéro de TVA" />
-        </InputGroup>
-      </div>
-    </template>
+            <div class="input-group">
+                <label>Numéro de SIRET</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-id-card"></i
+                    ></InputGroupAddon>
+                    <InputNumber
+                        v-model="pro.siret"
+                        placeholder="Numéro de SIRET"
+                    />
+                </InputGroup>
+            </div>
 
-    <!-- PARTICULIER -->
-    <template v-else>
-      <div class="input-group">
-        <label>Nom</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-user"></i></InputGroupAddon>
-          <InputText v-model="part.fullname" placeholder="Nom complet" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Numéro de TVA</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-percentage"></i
+                    ></InputGroupAddon>
+                    <InputNumber
+                        v-model="pro.vat"
+                        placeholder="Numéro de TVA"
+                    />
+                </InputGroup>
+            </div>
+        </template>
 
-      <div class="input-group">
-        <label>Prénom</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-user-plus"></i></InputGroupAddon>
-          <InputText v-model="part.firstname" placeholder="Prénom" />
-        </InputGroup>
-      </div>
+        <!-- PARTICULIER -->
+        <template v-else>
+            <div class="input-group">
+                <label>Nom</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-user"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="part.fullname"
+                        placeholder="Nom complet"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Email</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-envelope"></i></InputGroupAddon>
-          <InputText v-model="part.email" placeholder="Email" type="email" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Prénom</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-user-plus"></i
+                    ></InputGroupAddon>
+                    <InputText v-model="part.firstname" placeholder="Prénom" />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Nom d'utilisateur</label>
-        <InputGroup>
-          <InputGroupAddon>
-            <i class="pi pi-desktop"></i>
-          </InputGroupAddon>
-          <InputText v-model="part.username" placeholder="Nom utilisateur" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Email</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-envelope"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="part.email"
+                        placeholder="Email"
+                        type="email"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Téléphone</label>
-        <InputGroup>
-          <InputGroupAddon>
-            <span style="color: black">+33</span>
-          </InputGroupAddon>
-          <InputNumber v-model="part.phoneNumber" />
-        </InputGroup>
-      </div>
+            <div class="input-group">
+                <label>Nom d'utilisateur</label>
+                <InputGroup>
+                    <InputGroupAddon>
+                        <i class="pi pi-desktop"></i>
+                    </InputGroupAddon>
+                    <InputText
+                        v-model="part.username"
+                        placeholder="Nom utilisateur"
+                    />
+                </InputGroup>
+            </div>
 
-      <div class="input-group">
-        <label>Mot de passe</label>
-        <InputGroup>
-          <InputGroupAddon><i class="pi pi-lock"></i></InputGroupAddon>
-          <InputText
-            v-model="part.password"
-            type="password"
-            placeholder="Mot de passe"
-          />
-        </InputGroup>
-      </div>
-    </template>
+            <div class="input-group">
+                <label>Téléphone</label>
+                <InputGroup>
+                    <InputGroupAddon>
+                        <span style="color: black">+33</span>
+                    </InputGroupAddon>
+                    <InputNumber v-model="part.phoneNumber" />
+                </InputGroup>
+            </div>
 
-    <Button
-      class="button"
-      :label="loading ? 'S’inscrire…' : 'S’inscrire'"
-      :disabled="loading"
-      type="submit"
-    />
-    <p v-if="error" class="error">{{ error }}</p>
+            <div class="input-group">
+                <label>Mot de passe</label>
+                <InputGroup>
+                    <InputGroupAddon
+                        ><i class="pi pi-lock"></i
+                    ></InputGroupAddon>
+                    <InputText
+                        v-model="part.password"
+                        type="password"
+                        placeholder="Mot de passe"
+                    />
+                </InputGroup>
+            </div>
+        </template>
 
-    <p class="hint">
-      Vous avez déjà un compte ?
-      <NuxtLink to="/auth/login">Se connecter</NuxtLink>
-    </p>
-  </form>
+        <Button
+            class="button"
+            :label="loading ? 'S’inscrire…' : 'S’inscrire'"
+            :disabled="loading"
+            type="submit"
+        />
+        <p v-if="error" class="error">{{ error }}</p>
+
+        <p class="hint">
+            Vous avez déjà un compte ?
+            <NuxtLink to="/auth/login">Se connecter</NuxtLink>
+        </p>
+    </form>
 </template>
 
 <script setup lang="ts">
@@ -147,62 +198,64 @@ import { useAuth } from "~/composables/useAuth";
 
 type UserType = "pro" | "part";
 interface ProForm {
-  company: string;
-  email: string;
-  password: string;
-  siret: number | null;
-  vat: number | null;
+    company: string;
+    username: string;
+    email: string;
+    password: string;
+    siret: number | null;
+    vat: number | null;
 }
 
 interface PartForm {
-  fullname: string;
-  firstname: string;
-  username: string;
-  email: string;
-  password: string;
-  phoneNumber: string;
+    fullname: string;
+    firstname: string;
+    username: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
 }
 const { register, loading, error } = useAuth();
 
 const userType = ref<UserType>("part");
 
 const pro = ref<ProForm>({
-  company: "",
-  email: "",
-  password: "",
-  siret: null,
-  vat: null,
+    company: "",
+    username: "",
+    email: "",
+    password: "",
+    siret: null,
+    vat: null,
 });
 const part = ref<PartForm>({
-  fullname: "",
-  firstname: "",
-  email: "",
-  password: "",
-  phoneNumber: "",
+    fullname: "",
+    firstname: "",
+    email: "",
+    password: "",
+    phoneNumber: "",
 });
 
 const onSubmit = async () => {
-  const payload =
-    userType.value === "pro"
-      ? { type: "pro", ...pro.value }
-      : { type: "part", ...part.value };
-  // Validation minimale côté client (évite envoi vide)
-  if (
-    userType.value === "pro" &&
-    (!pro.value.company || !pro.value.email || !pro.value.password)
-  )
-    return;
-  if (
-    userType.value === "part" &&
-    (!part.value.fullname ||
-      !part.value.firstname ||
-      !part.value.email ||
-      !part.value.password)
-  )
-    return;
+    const payload =
+        userType.value === "pro"
+            ? { type: "pro", ...pro.value }
+            : { type: "part", ...part.value };
+    // Validation minimale côté client (évite envoi vide)
+    if (
+        userType.value === "pro" &&
+        (!pro.value.company || !pro.value.email || !pro.value.password)
+    )
+        return;
+    if (
+        userType.value === "part" &&
+        (!part.value.fullname ||
+            !part.value.firstname ||
+            !part.value.email ||
+            !part.value.password)
+    )
+        return;
 
-  await register(payload as any); // typé dans useAuth
-  await navigateTo("/auth/login")
+    await register(payload as any); // typé dans useAuth
+    await navigateTo("/auth/login");
 };
 </script>
 
