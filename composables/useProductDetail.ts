dbf2ccb70f883a27734fa39eb679af18616184b2
remy <remy.berjonneau@gmail.com>
2025-12-11 10:23:21 +0100

    url localhost forcer ^^

--- composables/useProductDetail.ts
+++ composables/useProductDetail.ts
@@ -1,19 +1,18 @@
-import { useProductStore } from '@/stores/useProductStore'
+import { useProductStore } from "@/stores/useProductStore";
 
 export const useProduct = (id: number | string) => {
-  const productStore = useProductStore()
-  
+  const productStore = useProductStore();
+
   const { data, error, pending } = useFetch(`/product/getProductById/${id}`, {
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.NUXT_PUBLIC_API_BASE,
     key: `product-${id}`,
     transform: (response) => response?.data || null,
     default: () => null,
-  })
-
+  });
 
   return {
     product: data,
     pending,
-    error
-  }
-}
+    error,
+  };
+};
